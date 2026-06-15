c7ecd17aad4f6fc4e2ce3e9c1e9d74d38000e915
raihanchoudhury898 <reset0196@gmail.com>
2026-06-15 12:57:51 +0600

    Update rules.js

--- scripts/cmds/rules.js
+++ scripts/cmds/rules.js
@@ -1,12 +1,12 @@
-const { getPrboxx } = global.utils;
+const { getPrefix } = global.utils || {};
 
 module.exports = {
 	config: {
 		name: "rules",
 		version: "1.6",
-		author: "NTKhang",
+		author: "Raihan Choudhury",
 		countDown: 5,
-		role: 0,
+		role: 1,
 		description: {
 			vi: "Tạo/xem/thêm/sửa/đổi vị trí/xóa nội quy nhóm của bạn",
 			en: "Create/view/add/edit/change position/delete group rules of you"
@@ -25,6 +25,7 @@ module.exports = {
 				+ "\n    {pn} move 1 3"
 				+ "\n    {pn} -e 1 không spam tin nhắn trong nhóm"
 				+ "\n    {pn} -r",
+
 			en: "   {pn} [add | -a] <rule to add>: add rule for group."
 				+ "\n   {pn}: view group rules."
 				+ "\n   {pn} [edit | -e] <n> <content after edit>: edit rule number n."
@@ -56,6 +57,10 @@ module.exports = {
 			noPermissionMove: "Chỉ quản trị viên mới có thể đổi vị trí nội quy của nhóm",
 			invalidNumberMove: "Vui lòng nhập số thứ tự của 2 nội quy nhóm bạn muốn chuyển đổi vị trí với nhau",
 			sameNumberMove: "Không thể chuyển đổi vị trí của 2 nội quy giống nhau",
+
+			// Bug Fix
+			rulesNotExistMove: "Không tồn tại nội quy thứ %1",
+
 			rulesNotExistMove2: "Không tồn tại nội quy thứ %1 và %2",
 			successMove: "Đã chuyển đổi vị trí của 2 nội quy thứ %1 và %2 thành công",
 			noPermissionDelete: "Chỉ quản trị viên mới có thể xóa nội quy của nhóm",
@@ -67,6 +72,7 @@ module.exports = {
 			successRemove: "Đã xóa toàn bộ nội quy của nhóm thành công",
 			invalidNumberView: "Vui lòng nhập số thứ tự của nội quy bạn muốn xem"
 		},
+
 		en: {
 			yourRules: "Your group rules\n%1",
 			noRules: "Your group has no rules, to add rules for group use `%1rules add`",
@@ -82,6 +88,10 @@ module.exports = {
 			noPermissionMove: "Only admins can move group rules",
 			invalidNumberMove: "Please enter the number of 2 group rules you want to swap",
 			sameNumberMove: "Cannot swap position of 2 same rules",
+
+			// Bug Fix
+			rulesNotExistMove: "Rule number %1 does not exist",
+
 			rulesNotExistMove2: "Rule number %1 and %2 does not exist",
 			successMove: "Swapped position of rule number %1 and %2 successfully",
 			noPermissionDelete: "Only admins can delete group rules",
@@ -94,8 +104,7 @@ module.exports = {
 			invalidNumberView: "Please enter the number of the rule you want to view"
 		}
 	},
-
-	onStart: async function ({ role, args, message, event, threadsData, getLang, commandName }) {
+onStart: async function ({ role, args, message, event, threadsData, getLang, commandName }) {
 		const { threadID, senderID } = event;
 
 		const type = args[0];
@@ -104,137 +113,315 @@ module.exports = {
 
 		if (!type) {
 			let i = 1;
-			const msg = rulesOfThread.reduce((text, rules) => text += `${i++}. ${rules}\n`, "");
-			message.reply(msg ? getLang("yourRules", msg) : getLang("noRules", getPrefix(threadID)), (err, info) => {
-				global.GoatBot.onReply.set(info.messageID, {
-					commandName,
-					author: senderID,
-					rulesOfThread,
-					messageID: info.messageID
-				});
-			});
+			const msg = rulesOfThread.reduce(
+				(text, rules) => text += `${i++}. ${rules}\n`,
+				""
+			);
+
+			message.reply(
+				msg
+					? getLang("yourRules", msg)
+					: getLang(
+						"noRules",
+						typeof getPrefix == "function"
+							? getPrefix(threadID)
+							: global.GoatBot.config.prefix
+					),
+				(err, info) => {
+					if (err)
+						return;
+
+					global.GoatBot.onReply.set(info.messageID, {
+						commandName,
+						author: senderID,
+						rulesOfThread,
+						messageID: info.messageID
+					});
+				}
+			);
 		}
+
 		else if (["add", "-a"].includes(type)) {
 			if (role < 1)
 				return message.reply(getLang("noPermissionAdd"));
+
 			if (!args[1])
 				return message.reply(getLang("noContent"));
+
 			rulesOfThread.push(args.slice(1).join(" "));
+
 			try {
-				await threadsData.set(threadID, rulesOfThread, "data.rules");
+				await threadsData.set(
+					threadID,
+					rulesOfThread,
+					"data.rules"
+				);
+
 				message.reply(getLang("success"));
 			}
 			catch (err) {
 				message.err(err);
 			}
 		}
+
 		else if (["edit", "-e"].includes(type)) {
 			if (role < 1)
 				return message.reply(getLang("noPermissionEdit"));
+
 			const stt = parseInt(args[1]);
-			if (stt === NaN)
+
+			// Bug Fixed
+			if (isNaN(stt))
 				return message.reply(getLang("invalidNumber"));
-			if (!rulesOfThread[stt - 1])
-				return message.reply(`${getLang("rulesNotExist", stt)}, ${totalRules == 0 ? getLang("noRules") : getLang("numberRules", totalRules)}`);
+
+			if (!rulesOfThread[stt - 1]) {
+				return message.reply(
+					`${getLang("rulesNotExist", stt)}, ${
+						totalRules == 0
+							? getLang("noRules", global.GoatBot.config.prefix)
+							: getLang("numberRules", totalRules)
+					}`
+				);
+			}
+
 			if (!args[2])
-				return message.reply(getLang("noContentEdit", stt));
+				return message.reply(
+					getLang("noContentEdit", stt)
+				);
+
 			const newContent = args.slice(2).join(" ");
+
 			rulesOfThread[stt - 1] = newContent;
+
 			try {
-				await threadsData.set(threadID, rulesOfThread, "data.rules");
-				message.reply(getLang("successEdit", stt, newContent));
+				await threadsData.set(
+					threadID,
+					rulesOfThread,
+					"data.rules"
+				);
+
+				message.reply(
+					getLang(
+						"successEdit",
+						stt,
+						newContent
+					)
+				);
 			}
 			catch (err) {
 				message.err(err);
 			}
 		}
-		else if (["move", "-m"].includes(type)) {
+else if (["move", "-m"].includes(type)) {
 			if (role < 1)
 				return message.reply(getLang("noPermissionMove"));
+
 			const num1 = parseInt(args[1]);
 			const num2 = parseInt(args[2]);
+
 			if (isNaN(num1) || isNaN(num2))
 				return message.reply(getLang("invalidNumberMove"));
+
 			if (!rulesOfThread[num1 - 1] || !rulesOfThread[num2 - 1]) {
-				let msg = !rulesOfThread[num1 - 1] ?
-					!rulesOfThread[num2 - 1] ?
-						message.reply(getLang("rulesNotExistMove2", num1, num2)) :
-						message.reply(getLang("rulesNotExistMove", num1)) :
-					message.reply(getLang("rulesNotExistMove", num2));
-				msg += `, ${totalRules == 0 ? getLang("noRules") : getLang("numberRules", totalRules)}`;
+				let msg = "";
+
+				if (!rulesOfThread[num1 - 1] && !rulesOfThread[num2 - 1])
+					msg = getLang("rulesNotExistMove2", num1, num2);
+				else if (!rulesOfThread[num1 - 1])
+					msg = getLang("rulesNotExistMove", num1);
+				else
+					msg = getLang("rulesNotExistMove", num2);
+
+				msg += `, ${
+					totalRules == 0
+						? getLang("noRules", global.GoatBot.config.prefix)
+						: getLang("numberRules", totalRules)
+				}`;
+
 				return message.reply(msg);
 			}
+
 			if (num1 == num2)
 				return message.reply(getLang("sameNumberMove"));
 
-			// swap
-			[rulesOfThread[num1 - 1], rulesOfThread[num2 - 1]] = [rulesOfThread[num2 - 1], rulesOfThread[num1 - 1]];
+			[rulesOfThread[num1 - 1], rulesOfThread[num2 - 1]] = [
+				rulesOfThread[num2 - 1],
+				rulesOfThread[num1 - 1]
+			];
+
 			try {
-				await threadsData.set(threadID, rulesOfThread, "data.rules");
-				message.reply(getLang("successMove", num1, num2));
+				await threadsData.set(
+					threadID,
+					rulesOfThread,
+					"data.rules"
+				);
+
+				message.reply(
+					getLang("successMove", num1, num2)
+				);
 			}
 			catch (err) {
 				message.err(err);
 			}
 		}
+
 		else if (["delete", "del", "-d"].includes(type)) {
 			if (role < 1)
 				return message.reply(getLang("noPermissionDelete"));
+
 			if (!args[1] || isNaN(args[1]))
-				return message.reply(getLang("invalidNumberDelete"));
-			const rulesDel = rulesOfThread[parseInt(args[1]) - 1];
-			if (!rulesDel)
-				return message.reply(`${getLang("rulesNotExistDelete", args[1])}, ${totalRules == 0 ? getLang("noRules") : getLang("numberRules", totalRules)}`);
-			rulesOfThread.splice(parseInt(args[1]) - 1, 1);
-			await threadsData.set(threadID, rulesOfThread, "data.rules");
-			message.reply(getLang("successDelete", args[1], rulesDel));
+				return message.reply(
+					getLang("invalidNumberDelete")
+				);
+
+			const rulesDel =
+				rulesOfThread[parseInt(args[1]) - 1];
+
+			if (!rulesDel) {
+				return message.reply(
+					`${getLang(
+						"rulesNotExistDelete",
+						args[1]
+					)}, ${
+						totalRules == 0
+							? getLang("noRules", global.GoatBot.config.prefix)
+							: getLang("numberRules", totalRules)
+					}`
+				);
+			}
+
+			rulesOfThread.splice(
+				parseInt(args[1]) - 1,
+				1
+			);
+
+			await threadsData.set(
+				threadID,
+				rulesOfThread,
+				"data.rules"
+			);
+
+			message.reply(
+				getLang(
+					"successDelete",
+					args[1],
+					rulesDel
+				)
+			);
 		}
-		else if (["remove", "reset", "-r", "-rm"].includes(type)) {
+
+		else if (
+			["remove", "reset", "-r", "-rm"]
+				.includes(type)
+		) {
 			if (role < 1)
-				return message.reply(getLang("noPermissionRemove"));
-			message.reply(getLang("confirmRemove"), (err, info) => {
-				global.GoatBot.onReaction.set(info.messageID, {
-					commandName: "rules",
-					messageID: info.messageID,
-					author: senderID
-				});
-			});
+				return message.reply(
+					getLang("noPermissionRemove")
+				);
+
+			message.reply(
+				getLang("confirmRemove"),
+				(err, info) => {
+					if (err)
+						return;
+
+					global.GoatBot.onReaction.set(
+						info.messageID,
+						{
+							commandName: "rules",
+							messageID: info.messageID,
+							author: senderID
+						}
+					);
+				}
+			);
 		}
-		else if (!isNaN(type)) {
+else if (!isNaN(type)) {
 			let msg = "";
+
 			for (const stt of args) {
-				const rules = rulesOfThread[parseInt(stt) - 1];
+				const rules =
+					rulesOfThread[parseInt(stt) - 1];
+
 				if (rules)
 					msg += `${stt}. ${rules}\n`;
 			}
-			if (msg == "")
-				return message.reply(`${getLang("rulesNotExist", type)}, ${totalRules == 0 ? getLang("noRules") : getLang("numberRules", totalRules)}`);
+
+			if (msg == "") {
+				return message.reply(
+					`${getLang("rulesNotExist", type)}, ${
+						totalRules == 0
+							? getLang("noRules", global.GoatBot.config.prefix)
+							: getLang("numberRules", totalRules)
+					}`
+				);
+			}
+
 			message.reply(msg);
 		}
+
 		else {
 			message.SyntaxError();
 		}
 	},
 
-	onReply: async function ({ message, event, getLang, Reply }) {
+	onReply: async function ({
+		message,
+		event,
+		getLang,
+		Reply
+	}) {
 		const { author, rulesOfThread } = Reply;
+
 		if (author != event.senderID)
 			return;
+
 		const num = parseInt(event.body || "");
+
 		if (isNaN(num) || num < 1)
-			return message.reply(getLang("invalidNumberView"));
+			return message.reply(
+				getLang("invalidNumberView")
+			);
+
 		const totalRules = rulesOfThread.length;
-		if (num > totalRules)
-			return message.reply(`${getLang("rulesNotExist", num)}, ${totalRules == 0 ? getLang("noRules") : getLang("numberRules", totalRules)}`);
-		message.reply(`${num}. ${rulesOfThread[num - 1]}`, () => message.unsend(Reply.messageID));
+
+		if (num > totalRules) {
+			return message.reply(
+				`${getLang("rulesNotExist", num)}, ${
+					totalRules == 0
+						? getLang("noRules", global.GoatBot.config.prefix)
+						: getLang("numberRules", totalRules)
+				}`
+			);
+		}
+
+		message.reply(
+			`${num}. ${rulesOfThread[num - 1]}`,
+			() => message.unsend(Reply.messageID)
+		);
 	},
 
-	onReaction: async ({ threadsData, message, Reaction, event, getLang }) => {
+	onReaction: async function ({
+		threadsData,
+		message,
+		Reaction,
+		event,
+		getLang
+	}) {
 		const { author } = Reaction;
 		const { threadID, userID } = event;
+
 		if (author != userID)
 			return;
-		await threadsData.set(threadID, [], "data.rules");
-		message.reply(getLang("successRemove"));
+
+		await threadsData.set(
+			threadID,
+			[],
+			"data.rules"
+		);
+
+		message.reply(
+			getLang("successRemove")
+		);
 	}
 };
